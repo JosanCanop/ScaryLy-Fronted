a4e0540dc0ddd192e49112d5e66725b53d5276ef
JosanCanop <josancanop@gmail.com>
2023-05-01 17:18:42 +0200

    añadido formulario en todos los footer con enlace a nueva pestaña

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ footer.innerHTML = `<div class="flex items-center justify-center">
 </div>
 <div class="flex items-center justify-between px-4 mt-4 text-white">
 <div class="sm:basis-4/12 text-xs sm:text-base">
-    <a href="formSugerencias.html">
+    <a href="https://tally.so/r/mKzdyM" target="_blank">
         <p class="py-1 mb-4 text-center">Envianos tus comentarios</p>
     </a>
 </div>
