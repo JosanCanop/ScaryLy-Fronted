d08992485a011340c6a49c591c058eb19fa3174f
JosanCanop <josancanop@gmail.com>
2023-05-03 13:54:58 +0200

    compleato botones dinamicos de genero

--- js/home.js
+++ js/home.js
@@ -1,9 +1,10 @@
 import { carousel } from "./carrousel.js";
-import { checkTokenOff } from "./tokenoff.js";
+//import { checkTokenOff } from "./tokenoff.js";
 
-checkTokenOff()
+//checkTokenOff()
+let genreIdPassed = false;
+
 async function getMovies(idGenero) {
-    console.log(idGenero)
     try {
         let myHeaders = new Headers();
         myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
@@ -14,7 +15,7 @@ async function getMovies(idGenero) {
 
         let url
         if (idGenero) {
-            url = `http://localhost:1337/api/genres/` + idGenero + `?populate=*`
+            url = `http://localhost:1337/api/genres/?populate=*&filters[id][$eq]=${idGenero}`
         } else {
             url = `http://localhost:1337/api/genres?populate=*`
         }
@@ -24,7 +25,7 @@ async function getMovies(idGenero) {
 
             if (response.status == 401) {
                 localStorage.removeItem("token")
-                window.location.href = "login.html"
+                window.location.href = "index.html"
             } else {
                 const message = `Error: ${response.status}`;
                 throw new Error(message);
@@ -68,18 +69,39 @@ async function getGenres() {
     }
 }
 
+function toggleGetMovies(genreId, button) {
+    if (genreIdPassed) {
+        getMovies();
+        genreIdPassed = false;
+        button.classList.remove("focus:bg-red-900");
+    } else {
+        getMovies(genreId);
+        genreIdPassed = true;
+        button.classList.add("focus:bg-red-900");
+    }
+}
 
 function showGenreButtons(data) {
+    //onclick="getMovies(${genre.id})"
     const buttonsGenres = document.getElementById("btnGenres")
+    let genreId = null;
     for (let genre of data.data) {
-        buttonsGenres.innerHTML += `<button id="buton-${genre.id}" onclick="getMovies(${genre.id})"
-            class=" m-1 hover:font-semibold border border-double  bg-opacity-70 border-white rounded-full px-3 py-2 hover:bg-red-900 text-white">${genre.attributes.name}</button>`
-    }
+        buttonsGenres.innerHTML += `<button id="button-${genre.id}"
+            class="generosPelis m-1 hover:font-semibold border border-double  bg-opacity-70 border-white rounded-full px-3 py-2 hover:bg-red-900 text-white">${genre.attributes.name}</button>`
 
+    }
+    let buttonsGenre = document.querySelectorAll(".generosPelis")
+    for (let button of buttonsGenre) {
+        button.addEventListener("click", async () => {
+            genreId = button.id.substring(7)
+            toggleGetMovies(genreId, button)
+        }, false)
+    }
 }
 
 function showMoviesGenre(data) {
     const carouselDinamic = document.getElementById("carouselDinamic")
+    carouselDinamic.innerHTML = ""
     for (let genre of data.data) {
         let peliculas = ""
         for (const pelicula of genre.attributes.movies.data) {
@@ -87,6 +109,7 @@ function showMoviesGenre(data) {
         <a href="http://127.0.0.1:5501/detailmovie.html?id=${pelicula.id}"><img src="${pelicula.attributes.image}" alt=""></a>
     </div>`
         }
+
         carouselDinamic.innerHTML += `<div id="padre-${genre.id}" class="contenedor-titulo-controles">
         <h3>${genre.attributes.name}</h3>
         <div class="indicadores"></div>
