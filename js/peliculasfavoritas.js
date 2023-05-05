de8454755a45a70824e52deba718749f61222918
Valeria <valeriapolo96@gmail.com>
2023-05-05 20:42:52 +0200

    avance js lista de peliculas favoritas

--- js/peliculasfavoritas.js
+++ js/peliculasfavoritas.js
@@ -32,31 +32,45 @@ async function getMoviesLikesUser() {
     }
 }
 
-let allLikes
+let arrayAllLikes = [];
+let movieId;
 
 function showMoviesUser(data) {
-    allLikes = data.likes
-    document.getElementById("contadorLikes").innerHTML = '(' + allLikes.length + ')'
+    document.getElementById("contadorLikes").innerHTML = '(' + arrayAllLikes.length + ')'
     const moviesLikes = document.getElementById("moviesLikes")
     for (const movie of data.likes) {
+        arrayAllLikes.push(movie.id);
+
         moviesLikes.innerHTML += `<div class="pelicula">
     <a href="http://127.0.0.1:5501/detailmovie.html?id=${movie.id}">
         <figure class="snip0023 rounded-lg">
             <img src="${movie.image}" alt="">
             <div>
-                <button onclick=""><i class="ion-ios-trash-outline text-xs"></i></button>
+                <button class="idButton" id="btn-${movie.id}"><i class="ion-ios-trash-outline text-xs"></i></button>
                 <div class="curl"></div>
             </div>
         </figure>
     </a>
 </div>`
+        const btnDelete = document.getElementById(`btn-${movie.id}`);
+        btnDelete.addEventListener("click", () => {
+            remove()
+        });
     }
+    let buttonsDelete = document.querySelectorAll(".idButton")
+    movieId;
+    for (const button of buttonsDelete) {
+        let movieId = button.id.substring(4);
+        button.addEventListener("click", () => {
+            remove(movieId);
+        });
+    }
+
 }
 
 getMoviesLikesUser()
 
 
-/*
 async function updateProfile(data) {
     try {
         const response = await fetch(urlBase + '/users/' + localStorage.getItem("idUser"), {
@@ -71,7 +85,7 @@ async function updateProfile(data) {
             const message = `Error: ${response.status}`;
             throw new Error(message);
         }
-        alert("borrar¿?")
+        //alert("borrar¿?")
         location.reload()
     } catch (error) {
         console.log(error)
@@ -79,48 +93,20 @@ async function updateProfile(data) {
 }
 
 function remove() {
-    //borrar bicis de la lista
-    if (myBikes.selectedIndex != -1) {
-        var value = myBikes.value;
-        var selectedOption = myBikes.options[myBikes.selectedIndex];
-        const text = selectedOption.text;
-        selectedOption.classList.add("d-none")
-
-        for (var i = 0; i < arrayMyBikes.length; i++) {
-            if (arrayMyBikes[i].id == value) {
-                arrayMyBikes.splice(i, 1);
-            }
+    arrayAllLikes;
+    for (var i = 0; i < arrayAllLikes.length; i++) {
+        if (arrayAllLikes[i].id == movieId) {
+            arrayAllLikes.splice(i, 1);
         }
-
-        console.log(arrayMyBikes)
-
     }
-}
-
-function save() {
-    const name = nameInput.value
-    const surname = surnameInput.value
-
-    //getElement name, surname, array de bicicletas
-    //const array1 = [1, 4, 9, 16];
-
-    // Pass a function to map
-    //const map1 = array1.map(x => x.id);
-
-    //console.log(map1);
-    // Expected output: Array [2, 8, 18, 32]
-
-    const myBikesIds = arrayMyBikes.map(x => x.id);
-    //myBikedIds es un array, lo crea el map
+    const myLikesIds = arrayAllLikes.map(x => x.id);
 
     var raw = JSON.stringify({
-        "name": name,
-        "surname": surname,
-        "bicicletas": myBikesIds
+        "likes": myLikesIds
     })
-//console.log(raw)
-    /*o de la manera tradicional
+    updateProfile(raw)
+}
 
-    */
-   // updateProfile(raw)
-//}
+
+
+
